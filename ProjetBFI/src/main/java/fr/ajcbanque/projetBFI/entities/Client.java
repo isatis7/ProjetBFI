27eaa0232bf7e16e2a362ed4935fd1e447395bb0
Xavier Tang <xavier.tang@hotmail.fr>
2018-09-04 15:50:15 +0200

    Client V1

--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/Client.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/Client.java
@@ -6,6 +6,9 @@ public class Client implements Serializable {
     private Long	   id;
     private String	   nom;
     private FormeJuridique formeJuridique;
+    private Pays	   pays;
+    private Long	   numCarteBancaire;
+    private RatingInterne  ratingInterne;
 
     public static enum FormeJuridique {
 	SAS, SASU, SARL, SA;
