ddb486755a2e4b161c4b5aec7311becd226f1f8f
Xavier Tang <xavier.tang@hotmail.fr>
2018-09-04 17:04:46 +0200

    Entities version finale : mapping + getter/setter + hashcode/equals + toString

--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/Collaborateur.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/Collaborateur.java
@@ -7,18 +7,141 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Collaborateur implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8494510760349250205L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long   id;
+    private Long	      id;
+    @NotNull(message = "{error.commons.required}")
     @Column(length = 10, nullable = false, unique = true)
-    private String matriculeInterne;
+    private String	      matriculeInterne;
+    @NotNull(message = "{error.commons.required}")
+    @Column(length = 255, nullable = false)
+    private String	      nom;
+    @NotNull(message = "{error.commons.required}")
+    @Column(length = 255, nullable = false)
+    private String	      prenom;
+    @NotNull(message = "{error.commons.required}")
     @Column(length = 255, nullable = false, unique = true)
-    private String nom;
-    @Column(length = 255, nullable = false, unique = true)
-    private String prenom;
-    @Column(length = 255, nullable = false, unique = true)
-    private String email;
+    private String	      email;
+
+    public Collaborateur() {
+	//
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getMatriculeInterne() {
+	return matriculeInterne;
+    }
+
+    public void setMatriculeInterne(String matriculeInterne) {
+	this.matriculeInterne = matriculeInterne;
+    }
+
+    public String getNom() {
+	return nom;
+    }
+
+    public void setNom(String nom) {
+	this.nom = nom;
+    }
+
+    public String getPrenom() {
+	return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+	this.prenom = prenom;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((matriculeInterne == null) ? 0
+		: matriculeInterne.hashCode());
+	result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+	result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	Collaborateur other = (Collaborateur) obj;
+	if (email == null) {
+	    if (other.email != null) {
+		return false;
+	    }
+	} else if (!email.equals(other.email)) {
+	    return false;
+	}
+	if (id == null) {
+	    if (other.id != null) {
+		return false;
+	    }
+	} else if (!id.equals(other.id)) {
+	    return false;
+	}
+	if (matriculeInterne == null) {
+	    if (other.matriculeInterne != null) {
+		return false;
+	    }
+	} else if (!matriculeInterne.equals(other.matriculeInterne)) {
+	    return false;
+	}
+	if (nom == null) {
+	    if (other.nom != null) {
+		return false;
+	    }
+	} else if (!nom.equals(other.nom)) {
+	    return false;
+	}
+	if (prenom == null) {
+	    if (other.prenom != null) {
+		return false;
+	    }
+	} else if (!prenom.equals(other.prenom)) {
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "Collaborateur [id=" + id + ", matriculeInterne="
+		+ matriculeInterne + ", nom=" + nom + ", prenom=" + prenom
+		+ ", email=" + email + "]";
+    }
 }
--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/DemandeFinancement.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/DemandeFinancement.java
@@ -39,10 +39,195 @@ public class DemandeFinancement implements Serializable {
     @NotNull(message = "{error.commons.required}")
     @Column(nullable = false)
     private Float	      montant;
+    @ManyToOne
     @NotNull(message = "{error.commons.required}")
     @Column(nullable = false)
     private Devise	      devise;
+    @ManyToOne
     @NotNull(message = "{error.commons.required}")
     @Column(nullable = false)
     private TypeFinancement   typeFinancement;
+
+    public DemandeFinancement() {
+	//
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public Client getClient() {
+	return client;
+    }
+
+    public void setClient(Client client) {
+	this.client = client;
+    }
+
+    public LocalDate getDateDemande() {
+	return dateDemande;
+    }
+
+    public void setDateDemande(LocalDate dateDemande) {
+	this.dateDemande = dateDemande;
+    }
+
+    public String getReference() {
+	return reference;
+    }
+
+    public void setReference(String reference) {
+	this.reference = reference;
+    }
+
+    public Float getDuree() {
+	return duree;
+    }
+
+    public void setDuree(Float duree) {
+	this.duree = duree;
+    }
+
+    public LocalDate getDateEffective() {
+	return dateEffective;
+    }
+
+    public void setDateEffective(LocalDate dateEffective) {
+	this.dateEffective = dateEffective;
+    }
+
+    public Float getMontant() {
+	return montant;
+    }
+
+    public void setMontant(Float montant) {
+	this.montant = montant;
+    }
+
+    public Devise getDevise() {
+	return devise;
+    }
+
+    public void setDevise(Devise devise) {
+	this.devise = devise;
+    }
+
+    public TypeFinancement getTypeFinancement() {
+	return typeFinancement;
+    }
+
+    public void setTypeFinancement(TypeFinancement typeFinancement) {
+	this.typeFinancement = typeFinancement;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((client == null) ? 0 : client.hashCode());
+	result = prime * result
+		+ ((dateDemande == null) ? 0 : dateDemande.hashCode());
+	result = prime * result
+		+ ((dateEffective == null) ? 0 : dateEffective.hashCode());
+	result = prime * result + ((devise == null) ? 0 : devise.hashCode());
+	result = prime * result + ((duree == null) ? 0 : duree.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((montant == null) ? 0 : montant.hashCode());
+	result = prime * result
+		+ ((reference == null) ? 0 : reference.hashCode());
+	result = prime * result
+		+ ((typeFinancement == null) ? 0 : typeFinancement.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	DemandeFinancement other = (DemandeFinancement) obj;
+	if (client == null) {
+	    if (other.client != null) {
+		return false;
+	    }
+	} else if (!client.equals(other.client)) {
+	    return false;
+	}
+	if (dateDemande == null) {
+	    if (other.dateDemande != null) {
+		return false;
+	    }
+	} else if (!dateDemande.equals(other.dateDemande)) {
+	    return false;
+	}
+	if (dateEffective == null) {
+	    if (other.dateEffective != null) {
+		return false;
+	    }
+	} else if (!dateEffective.equals(other.dateEffective)) {
+	    return false;
+	}
+	if (devise == null) {
+	    if (other.devise != null) {
+		return false;
+	    }
+	} else if (!devise.equals(other.devise)) {
+	    return false;
+	}
+	if (duree == null) {
+	    if (other.duree != null) {
+		return false;
+	    }
+	} else if (!duree.equals(other.duree)) {
+	    return false;
+	}
+	if (id == null) {
+	    if (other.id != null) {
+		return false;
+	    }
+	} else if (!id.equals(other.id)) {
+	    return false;
+	}
+	if (montant == null) {
+	    if (other.montant != null) {
+		return false;
+	    }
+	} else if (!montant.equals(other.montant)) {
+	    return false;
+	}
+	if (reference == null) {
+	    if (other.reference != null) {
+		return false;
+	    }
+	} else if (!reference.equals(other.reference)) {
+	    return false;
+	}
+	if (typeFinancement == null) {
+	    if (other.typeFinancement != null) {
+		return false;
+	    }
+	} else if (!typeFinancement.equals(other.typeFinancement)) {
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "DemandeFinancement [id=" + id + ", client=" + client
+		+ ", dateDemande=" + dateDemande + ", reference=" + reference
+		+ ", duree=" + duree + ", dateEffective=" + dateEffective
+		+ ", montant=" + montant + ", devise=" + devise
+		+ ", typeFinancement=" + typeFinancement + "]";
+    }
 }
