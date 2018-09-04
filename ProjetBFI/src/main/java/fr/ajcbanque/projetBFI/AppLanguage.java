16836d67083825d223da4a0a1f433d7bf8b4a2c6
Benoit Mercier <ptitbebe17@hotmail.fr>
2018-09-04 17:16:57 +0200

    AppLanguages V1

---
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/AppLanguage.java
@@ -0,0 +1,48 @@
+package fr.ajcbanque.projetBFI;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public enum AppLanguage {
+    FR(Locale.FRENCH, "dd/MM/yyyy"), EN(Locale.ENGLISH, "MM/dd/yyyy");
+    public static final String			  CODE = "lang";
+    private static final Map<Locale, AppLanguage> LOCALES;
+    static {
+	LOCALES = new HashMap<>();
+	LOCALES.put(Locale.FRENCH, AppLanguage.FR);
+	LOCALES.put(Locale.ENGLISH, AppLanguage.EN);
+    }
+    private final Locale locale;
+    private final String datePattern;
+
+    private AppLanguage(Locale locale, String datePattern) {
+	this.locale = locale;
+	this.datePattern = datePattern;
+    }
+
+    public static AppLanguage valueOfLocale(Locale locale) {
+	AppLanguage result = LOCALES.get(locale);
+	return null == result ? getDefault() : result;
+    }
+
+    public static AppLanguage getDefault() {
+	return FR;
+    }
+
+    public Locale getLocale() {
+	return locale;
+    }
+
+    public boolean isFrench() {
+	return FR.equals(this);
+    }
+
+    public boolean isEnglish() {
+	return EN.equals(this);
+    }
+
+    public String getDatePattern() {
+	return datePattern;
+    }
+}
