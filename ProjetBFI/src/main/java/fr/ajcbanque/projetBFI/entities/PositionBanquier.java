6c7ae6b43758ba6bcab590cda35174abd0df141e
Benoit Mercier <ptitbebe17@hotmail.fr>
2018-09-05 10:41:51 +0200

    Annotations correction 1

--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/PositionBanquier.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/entities/PositionBanquier.java
@@ -2,11 +2,11 @@ package fr.ajcbanque.projetBFI.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
@@ -21,11 +21,11 @@ public class PositionBanquier implements Serializable {
     private Long	      id;
     @OneToOne
     @NotNull(message = "{error.commons.required}")
-    @Column(nullable = false)
+    @JoinColumn(nullable = false)
     private Collaborateur     collaborateur;
     @OneToOne
     @NotNull(message = "{error.commons.required}")
-    @Column(nullable = false)
+    @JoinColumn(nullable = false)
     private Client	      client;
 
     public PositionBanquier() {
