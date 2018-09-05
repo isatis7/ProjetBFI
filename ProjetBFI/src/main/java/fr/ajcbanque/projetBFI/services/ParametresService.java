c40eefec520ca4364ced1aa793cd34d9b4c9b04d
Thomas <thomassegura@gmail.com>
2018-09-05 16:14:16 +0200

    retiré couche de service Parametres et PerfPlus pour le moment

--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/services/ParametresService.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/services/ParametresService.java
@@ -1,21 +1,7 @@
 package fr.ajcbanque.projetBFI.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.ajcbanque.projetBFI.entities.Parametres;
-
 @Service
-public class ParametresService implements IParametresService {
-    @Autowired
-    private IParametresJpaRepository paramJpaRepo;
-
-    @Override
-    public void save(Parametres param) {
-	paramJpaRepo.save(param);
-    }
-
-    public Parametres getParam() {
-	return paramJpaRepo.getOne(1);
-    }
+public class ParametresService {
 }
