f7324978932bea201a6655c95d035f6e05f8c6a5
Benoit Mercier <ptitbebe17@hotmail.fr>
2018-09-06 14:30:11 +0200

    Correction DemandeController + Création ClientRepo

--- ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/repositories/ClientRepository.java
+++ ProjetBFI/src/main/java/fr/ajcbanque/projetBFI/repositories/ClientRepository.java
@@ -2,27 +2,23 @@ package fr.ajcbanque.projetBFI.repositories;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import fr.ajcbanque.projetBFI.AppLanguage;
 import fr.ajcbanque.projetBFI.dto.ClientDTO;
 
 public class ClientRepository extends BaseRepository
 			      implements IClientRepository {
+    @SuppressWarnings("unchecked")
     @Override
     public List<ClientDTO> findAllAsDTO(AppLanguage lang) {
 	StringBuilder queryBuilder = new StringBuilder(
-		"select new fr.formation.lamarmite.dto.CourseDTO(c.id, c.code, c.price, c.");
-	String nameCol = "frenchName as courseName, t.";
-	String typeNameCol = "frenchName";
-	if (lang.isEnglish()) {
-	    nameCol = "englishName as courseName, t.";
-	    typeNameCol = "englishName";
-	}
-	queryBuilder.append(nameCol);
-	queryBuilder.append(typeNameCol);
-	queryBuilder.append(") from Course c join c.type t");
-	queryBuilder.append(" order by t.code, courseName");
+		"select new fr.ajcbanque.ProjetBFI.dto.ClientDTO(");
+	queryBuilder.append(
+		"c.id, c.code, c.nom, c.formeJuridique, c.pays, c.numCompteBancaire, c.ratingInterne)");
+	queryBuilder.append(" from Client c");
+	queryBuilder.append(" order by c.nom");
 	Query query = em.createQuery(queryBuilder.toString());
 	return query.getResultList();
-    }
     }
 }
